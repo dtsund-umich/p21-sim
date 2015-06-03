66038c47f8871ce1e0d5ce4a63eeeb03158e813c
Derrick Sund <dtsund@email.arizona.edu>
2015-06-03 07:55:46 -0400

    Add the cruder, non-Fourier periodicity search to the repository.

---
+++ periodic_search_crude.py
@@ -0,0 +1,38 @@
+#Search the outputs of p21_sim, seeing if any of them exhibit periodicity
+#Cruder than periodic_search; no Fourier transforms here, just local max search
+
+import numpy
+from glob import glob
+import os
+
+directories = glob("*/")
+threshold = 0.1
+
+for d in directories:
+    if d == "./.git":
+        continue
+    os.chdir(d)
+    bad = False
+    for f in glob("*"):
+        reader = open(f, 'r')
+        lines = reader.readlines()
+        halfway  = len(lines) / 2
+        data = []
+        if lines[-1].split()[1].strip() == "nan":
+            print "Failure to converge found in trial " + d
+            bad = True
+            break
+        for line in lines[halfway:]:
+            data.append(float(line.split()[1].strip()))
+        maxima = []
+        for i in xrange(len(data)):
+            if i > 0 and i < len(data) - 1:
+                if data[i] > data[i-1] and data[i] > data[i+1]:
+                    maxima.append(i)
+        if max(data)/min(data) > 1.2 and len(maxima) > 2:
+            print "Periodic/bad behavior found in trial " + d + ", file " + f + ", period = " + str((maxima[1] - maxima[0]) * 0.01)
+            bad = True
+            break
+    if not bad:
+        print "Good behavior found in trial " + d
+    os.chdir("..")
