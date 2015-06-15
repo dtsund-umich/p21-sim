e6c2c1232205a34973ab5697363d536d4e475737
Derrick Sund <dtsund@email.arizona.edu>
2015-06-15 01:52:17 -0400

    Update p21_sim to track Rb-E7 complex as its own variable.

--- p21_sim.py
+++ p21_sim.py
@@ -56,6 +56,8 @@ alpha_ep21 = 1 #DUMMY with no physical basis XXX
 beta_rb = 0.7 #DUMMY with no physical basis XXX
 alpha_rb = 1 #DUMMY with no physical basis XXX
 alpha_crb = 1 #DUMMY with no physical basis XXX
+kappa_a = 1 #DUMMY with no physical basis XXX
+kappa_d = 1 #DUMMY with no physical basis XXX
 b_e7 = 1 #DUMMY with no physical basis XXX
 k_e = 0.01 #DUMMY with no physical basis XXX
 k_a = 0.01 #DUMMY with no physical basis XXX
@@ -96,12 +98,13 @@ md = k_dd * v_sd * gf / (k_gf + gf) / (v_dd - (v_sd * gf / (k_gf + gf)))
 #MDM2: Kim-Jackson
 #p21: XXX
 #Rb: XXX
+#Rb-E7: XXX, but starting at 0 makes a lot of sense
 #E2F: Goldbeter
 #Me: Goldbeter
 #Ma: Goldbeter
 #Mb: Goldbeter
 #Cdc20: Goldbeter
-y0 = [0.077,1.065,2.336,0.1,0.1,0.01,0.01,0.01,1.1,0.01]
+y0 = [0.077,1.065,2.336,0.1,0.1,0,0.01,0.01,0.01,1.1,0.01]
 
 #Potentially override parameters
 if infile != "":
@@ -124,11 +127,11 @@ def E7(t):
 #y[2] = MDM2
 #y[3] = p21
 #y[4] = Rb
-#y[5] = CDK1
-#y[6] = pMPF
-#y[7] = MPF
-#y[8] = Cyclin
-#y[9] = CDK1-P
+#y[6] = CDK1
+#y[7] = pMPF
+#y[8] = MPF
+#y[9] = Cyclin
+#y[10] = CDK1-P
 #y[10] = Cyclin-P
 names = []
 names.append("p53")
@@ -136,6 +139,7 @@ names.append("mdm2")
 names.append("MDM2")
 names.append("p21")
 names.append("Rb")
+names.append("Rb-E7")
 names.append("E2F")
 names.append("Me")
 names.append("Ma")
@@ -152,21 +156,23 @@ def func(y,t):
             #mdm2: p53 transcription + Rb transcription - degradation
             beta_mm * y[0] + beta_mi * kappa_rb / (kappa_rb + y[4]) - alpha_m * y[1],
             #MDM2: translation - degradation - MPF degradation
-            beta_m * y[1] - alpha_M * y[2] - alpha_mm * y[7] * y[2],
+            beta_m * y[1] - alpha_M * y[2] - alpha_mm * y[8] * y[2],
             #p21: Um, lots of things.
-            beta_p21 + beta_pp*y[0]/(y[0]+kappa_p) - alpha_p21*y[3] - alpha_ep21*E7(t)*y[3],
-            #Rb: synth - degrad - cyclin
-            beta_rb - alpha_rb*y[4] - alpha_crb*y[6]*y[4]**2/(y[4]+b_e7*E7(t)),
+            beta_p21 + beta_pp*y[0]/(y[0]+kappa_p) - alpha_p21*y[3] - alpha_ep21*(E7(t) - y[5])*y[3],
+            #Rb: synth - degrad - cyclin - E7 association + E7 dissociation
+            beta_rb - alpha_rb*y[4] - alpha_crb*y[7]*y[4] - kappa_a * y[4] * (E7(t) - y[5]) + kappa_d * y[5],
+            #Rb-E7: association - dissociation
+            kappa_a * y[4] * (E7(t) - y[5]) - kappa_d * y[5],
             #Active E2F: activation - deactivation
-            v_1e2f * (e2ftot - y[5])/(k_1e2f + e2ftot - y[5]) * (md + y[6]) - v_2e2f * y[5]/(k_2e2f + y[5]) * y[7],
+            v_1e2f * (e2ftot - y[6])/(k_1e2f + e2ftot - y[6]) * (md + y[7]) - v_2e2f * y[6]/(k_2e2f + y[6]) * y[8],
             #Cyclin E/CDK2 complex: synth - degrad (CycA/CDK2) - degrad (p21)
-            v_se * y[5] - v_de * y[7] * y[6]/(k_de + y[6]) - k_e * y[3] * y[6],
+            v_se * y[6] - v_de * y[8] * y[7]/(k_de + y[7]) - k_e * y[3] * y[7],
             #Cyclin A/CDK2 complex: synth - degrad (Cdc20) - degrad (p21)
-            v_sa * y[5] - v_da * y[9] * y[7]/(k_da + y[7]) - k_a * y[3] * y[7],
+            v_sa * y[6] - v_da * y[10] * y[8]/(k_da + y[8]) - k_a * y[3] * y[8],
             #Cyclin B/CDK1 complex: synth - degrad (Cdc20) - degrad (p21)
-            v_sb * y[7] - v_db * y[9] * y[8]/(k_db + y[8]) - k_b * y[3] * y[8],
+            v_sb * y[8] - v_db * y[10] * y[9]/(k_db + y[9]) - k_b * y[3] * y[9],
             #Active Cdc20: activation - deactivation
-            v_1cdc20 * y[8] * (cdc20tot - y[9])/(k_1cdc20 + cdc20tot - y[9]) - v_2cdc20 * y[9]/(k_2cdc20 + y[9])
+            v_1cdc20 * y[9] * (cdc20tot - y[10])/(k_1cdc20 + cdc20tot - y[10]) - v_2cdc20 * y[10]/(k_2cdc20 + y[10])
            ]
 
 t = arange(0, 500.0, 0.01)
