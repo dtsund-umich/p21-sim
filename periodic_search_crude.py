eff3cd554ea97155cbae339758404a78e2876306
Derrick Sund <dtsund@email.arizona.edu>
2015-08-30 15:46:21 -0400

    Add another robustness check.

--- periodic_search_crude.py
+++ periodic_search_crude.py
@@ -14,25 +14,29 @@ for d in directories:
     os.chdir(d)
     bad = False
     for f in glob("*"):
-        reader = open(f, 'r')
-        lines = reader.readlines()
-        halfway  = len(lines) / 2
-        data = []
-        if lines[-1].split()[1].strip() == "nan":
-            print "Failure to converge found in trial " + d
+        try:
+            reader = open(f, 'r')
+            lines = reader.readlines()
+            halfway  = len(lines) / 2
+            data = []
+            if lines[-1].split()[1].strip() == "nan":
+                print "Failure to converge found in trial " + d
+                bad = True
+                break
+            for line in lines[halfway:]:
+                data.append(float(line.split()[1].strip()))
+            maxima = []
+            for i in xrange(len(data)):
+                if i > 0 and i < len(data) - 1:
+                    if data[i] > data[i-1] and data[i] > data[i+1]:
+                        maxima.append(i)
+            if max(data) > 0 and max(data)/min(data) > 1.2 and len(maxima) > 2:
+                print "Periodic/bad behavior found in trial " + d + ", file " + f + ", period = " + str((maxima[1] - maxima[0]) * 0.01)
+                bad = True
+                break
+        except IndexError:
             bad = True
-            break
-        for line in lines[halfway:]:
-            data.append(float(line.split()[1].strip()))
-        maxima = []
-        for i in xrange(len(data)):
-            if i > 0 and i < len(data) - 1:
-                if data[i] > data[i-1] and data[i] > data[i+1]:
-                    maxima.append(i)
-        if max(data) > 0 and max(data)/min(data) > 1.2 and len(maxima) > 2:
-            print "Periodic/bad behavior found in trial " + d + ", file " + f + ", period = " + str((maxima[1] - maxima[0]) * 0.01)
-            bad = True
-            break
+            print "ERROR found in trial " + d
     if not bad:
         print "Good behavior found in trial " + d
     os.chdir("..")
