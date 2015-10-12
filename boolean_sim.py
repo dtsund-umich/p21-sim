86045b2a1873124e01446b064daa9326659c0b8f
Derrick Sund <dtsund@email.arizona.edu>
2015-10-12 12:58:23 -0400

    Finish gutting the hardcodedness from boolean_sim.

--- boolean_sim.py
+++ boolean_sim.py
@@ -36,43 +36,29 @@ text = generate(netstrings, names, initials)
 
 from boolean2 import Model
 
+pygame.init()
+
+
+space_needed = len(netstrings) * 70 + 10
+screen = pygame.display.set_mode((space_needed,850), DOUBLEBUF)
+
 model = boolean2.Model(text, mode='sync')
 model.initialize()
 model.iterate(steps=10)
-for state in model.states:
-    print state.Mb
 
-pygame.init()
-screen = pygame.display.set_mode((1024,850), DOUBLEBUF)
 screen.fill((255,255,255))
 font = pygame.font.Font(None, 30)
-screen.blit(font.render("p53", True, (0,0,0)), (10, 50))
-screen.blit(font.render("mdm2", True, (0,0,0)), (80, 50))
-screen.blit(font.render("MDM2", True, (0,0,0)), (150, 50))
-screen.blit(font.render("p21", True, (0,0,0)), (220, 50))
-screen.blit(font.render("Rb", True, (0,0,0)), (290, 50))
-screen.blit(font.render("E2F", True, (0,0,0)), (360, 50))
-screen.blit(font.render("p16", True, (0,0,0)), (430, 50))
-screen.blit(font.render("Md", True, (0,0,0)), (500, 50))
-screen.blit(font.render("Me", True, (0,0,0)), (570, 50))
-screen.blit(font.render("Ma", True, (0,0,0)), (640, 50))
-screen.blit(font.render("Mb", True, (0,0,0)), (710, 50))
-screen.blit(font.render("Cdc20", True, (0,0,0)), (780, 50))
-cur = 0
+for i in xrange(len(names)):
+    #Stagger the names, because some of them might be long
+    screen.blit(font.render(names[i], True, (0,0,0)), (10 + 70*i, 30 if i % 2 == 0 else 50))
+
+step = 0
 for state in model.states:
-    pygame.draw.rect(screen, (0,255,0) if state.p53 else (255,0,0),(10,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.mdm2rna else (255,0,0),(80,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.MDM2 else (255,0,0),(150,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.p21 else (255,0,0),(220,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.Rb else (255,0,0),(290,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.E2F else (255,0,0),(360,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.p16 else (255,0,0),(430,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.Md else (255,0,0),(500,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.Me else (255,0,0),(570,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.Ma else (255,0,0),(640,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.Mb else (255,0,0),(710,70+cur*70,70,70))
-    pygame.draw.rect(screen, (0,255,0) if state.Cdc20 else (255,0,0),(780,70+cur*70,70,70))
-    cur += 1
+    cur_var = 0
+    for name in names:
+        pygame.draw.rect(screen, (0,255,0) if getattr(state,name) else (255,0,0),(10+70*cur_var,70+step*70,70,70))
+        cur_var += 1
+    step += 1
 pygame.display.flip()
 raw_input()
 
