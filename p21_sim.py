4d06b9a1a91b6aa2969c14fca739551cc6decdec
Derrick Sund <dtsund@umich.edu>
2016-05-05 15:55:13 -0400

    Add Michaelis-Menten effects in degradation terms.

--- p21_sim.py
+++ p21_sim.py
@@ -84,6 +84,25 @@ sigma_CDC20 = 1 #DUMMY
 kappa_CECA = 1 #DUMMY
 kappa_CBCA = 1 #DUMMY
 
+k_p14 = 1
+k_Ink4 = 1
+k_p21 = 1
+k_p53 = 1
+k_RbMP = 1
+k_RbPP = 1
+k_RbCD = 1
+k_RbCE = 1
+k_E2FCA = 1
+k_E2F = 1
+k_mdm2r = 1
+k_MDM2 = 1
+k_CD = 1
+k_CE = 1
+k_CA = 1
+k_CB = 1
+k_CDC20CB = 1
+k_CDC20 = 1
+
 E2F_tot = 50 #DUMMY
 CDC20_tot = 0.1 #DUMMY
 
@@ -120,17 +139,17 @@ sanity_threshold = 0.00001
 def f(e2f, rb, rbmp):
     if e2f < sanity_threshold:
         return 0
-    return e2f**2 / (e2f + beta_E2FRb * rb + beta_E2FRbMP * rbmp)
+    return e2f / (e2f + beta_E2FRb * rb + beta_E2FRbMP * rbmp)
 
 def g(mdm2, p14):
     if mdm2 < sanity_threshold:
         return 0
-    return mdm2**2 / (mdm2 + beta_mdm2p14 * p14)
+    return mdm2 / (mdm2 + beta_mdm2p14 * p14)
 
 def h(c, p21, cd, ce, ca, cb):
     if c < sanity_threshold:
         return 0
-    return c**2 / (c + beta_cp21 * p21 * c / (ca+cb+cd+ce))
+    return c / (c + beta_cp21 * p21 * c / (ca+cb+cd+ce))
 
 #Variable key
 #y[0] = p14
@@ -173,49 +192,49 @@ def func(y,t):
     return [
             #We have p14 being produced by E2F after inhibition from Rb
             #is accounted for, and degraded at a constant rate.
-            alpha_p14 * f(y[7], y[4], y[5]) - omega_p14 * y[0],
+            alpha_p14 * f(y[7], y[4], y[5]) - omega_p14 * y[0]/(y[0] + k_p14),
             #It's just like the p14 equation, but with Ink4 instead!
-            alpha_Ink4 * f(y[7], y[4], y[5]) - omega_Ink4 * y[1],
+            alpha_Ink4 * f(y[7], y[4], y[5]) - omega_Ink4 * y[1]/(y[1] + k_Ink4),
             #Form p21 at a rate proportional to p53 presence; degrade it
             #"naturally" or with help from Cyclin E/CDK2.
-            alpha_p21 * y[3] - omega_p21 * y[2] - omega_p21CE * y[2] * y[11],
+            alpha_p21 * y[3] - y[2]/(y[2] + k_p21) * (omega_p21 - omega_p21CE * y[11]),
             #P53 is generated naturally at a constant rate, and degrades
             #both on its own and with help from MDM2.
-            alpha_p53 - omega_p53 * y[3] - omega_p53MDM2 * y[3] * g(y[9], y[0]),
+            alpha_p53 - y[3]/(y[3] + k_p53) * (omega_p53 - omega_p53MDM2 * g(y[9], y[0])),
             #Rb gets monophosphorylated by Cyclin D/CDK4-6.  Rb-monophosphate
             #gets its phosphate cleaved at a constant rate.
-            -epsilon_RbCD * y[4] * y[10] + sigma_Rb * y[5],
+            -epsilon_RbCD * y[4]/(y[4]+k_RbCD) * y[10] + sigma_Rb * y[5]/(y[5]+k_RbMP),
             #Rb-monophosphate can be formed by phosphorylation of Rb or cleavage
             #of Rb-polyphosphate.  It can be lost by Cyclin E/CDK2 or
             #phosphatase activity.
-            epsilon_RbCD * y[4] * y[10] - sigma_Rb * y[5] - epsilon_RbCE * y[5] * y[11] + sigma_RbMP * y[6],
+            epsilon_RbCD * y[4]/(y[4]+k_RbCD) * y[10] - sigma_Rb * y[5]/(y[5]+k_RbMP) - epsilon_RbCE * y[5]/(y[5]+k_RbCE) * y[11] + sigma_RbMP * y[6]/(y[6]+k_RbPP),
             #Rb-polyphosphate arises from Cyclin E/CDK2 activity on
             #Rb-monophosphate, and is lost by phosphatase activity.
-            epsilon_RbCE * y[5] * y[11] - sigma_RbMP * y[6],
+            epsilon_RbCE * y[5]/(y[5]+k_RbCE) * y[11] - sigma_RbMP * y[6]/(y[6]+k_RbPP),
             #E2F is inactivated by Cyclin A/CDK2.  It is reactivated at a
             #constant rate, or so this equation proposes.
-            -epsilon_E2F * y[7] * y[12] + sigma_E2F * (E2F_tot - y[7]),
+            -epsilon_E2F * y[7] * y[12]/(y[7] + k_E2FCA) + sigma_E2F * (E2F_tot - y[7])/(k_E2F + E2F_tot - y[7]),
             #mdm2 mRNA is promoted by p53 and degrades rapidly.
-            alpha_mdm2r * y[3] - omega_mdm2r * y[8],
+            alpha_mdm2r * y[3] - omega_mdm2r * y[8]/(y[8] + k_mdm2r),
             #MDM2 protein is translated from mdm2 mRNA, and is degraded at a
             #constant rate.
-            alpha_MDM2 * y[8] - omega_MDM2 * y[9],
+            alpha_MDM2 * y[8] - omega_MDM2 * y[9]/(y[9] + k_MDM2),
             #Cyclin D/CDK4-6 is promoted by E2F, and can degrade either on its
             #own or under the influence of Ink4.
-            alpha_CD * f(y[7], y[4], y[5]) - omega_CD * y[10] - omega_CDInk4 * y[10] * y[1],
+            alpha_CD * f(y[7], y[4], y[5]) - y[10]/(y[10] + k_CD) * (omega_CD - omega_CDInk4 * y[1]),
             #Cyclin E/CDK2 is also promoted by E2F, and degrades on its own.
             #When not inhibited by p21, it becomes Cyclin A/CDK2.
-            alpha_CE * f(y[7], y[4], y[5]) - omega_CE * y[11] - kappa_CECA * h(y[11], y[2], y[10], y[11], y[12], y[13]),
+            alpha_CE * f(y[7], y[4], y[5]) - omega_CE * y[11]/(y[11] + k_CE) - kappa_CECA * h(y[11], y[2], y[10], y[11], y[12], y[13]),
             #Cyclin A/CDK2 forms from Cyclin E/CDK2.  It degrades over time, and
             #degrades faster under the influence of active CDC20.
-            kappa_CECA * h(y[11], y[2], y[10], y[11], y[12], y[13]) - omega_CA * y[12] - omega_CACDC20 * y[12] * y[14],
+            kappa_CECA * h(y[11], y[2], y[10], y[11], y[12], y[13]) - y[12]/(y[12] + k_CA) * (omega_CA - omega_CACDC20 * y[14]),
             #Cyclin B/CDK1 is constantly produced, but normally gets degraded
             #quickly; active Cyclin A/CDK2 slows down the degradation.  Active
             #CDC20 also degrades it, however.
-            alpha_CB - omega_CB * y[13] * 1/(kappa_CBCA + h(y[12], y[2], y[10], y[11], y[12], y[13])) - omega_CBCDC20 * y[13] * y[14],
+            alpha_CB - y[13]/(y[13] + k_CB) * (omega_CB * 1/(kappa_CBCA + h(y[12], y[2], y[10], y[11], y[12], y[13])) - omega_CBCDC20 * y[14]),
             #CDC20 is activated by Cyclin B/CDK1.  It is inactivated gradually
             #over time.
-            sigma_CDC20 * y[13] * (CDC20_tot - y[14]) - epsilon_CDC20 * y[14],
+            sigma_CDC20 * y[13] * (CDC20_tot - y[14])/(k_CDC20CB + CDC20_tot - y[14]) - epsilon_CDC20 * y[14]/(k_CDC20 + y[14]),
            ]
 
 t = arange(0, 2000.0, 0.1)
