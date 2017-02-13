7d63a2ceac7c830b5a819296d5c9180f53ac45ce
dtsund <dtsund@umich.edu>
2017-02-13 01:56:52 -0500

    Continued work on the Fourier transform periodicity search, now mostly works.

--- periodic_search.py
+++ periodic_search.py
@@ -1,8 +1,10 @@
 #Search the outputs of p21_sim, seeing if any of them exhibit periodicity
 
 import numpy
+from matplotlib import pyplot as plt
 from glob import glob
 import os
+import sys
 
 directories = glob("*/")
 threshold = 0.1
@@ -12,7 +14,10 @@ for d in directories:
         continue
     os.chdir(d)
     bad = False
+    periods = []
     for f in glob("*"):
+        if "scaled" in f:
+            continue
         reader = open(f, 'r')
         lines = reader.readlines()
         halfway  = len(lines) / 2
@@ -21,15 +26,20 @@ for d in directories:
             print "Failure to converge found in trial " + d
             bad = True
             break
-        for line in lines[halfway:]:
+        for line in lines:
             data.append(float(line.split()[1].strip()))
+        stepsize = 0.1 #FIXME
         transformed = numpy.fft.rfft(data)
-        transformed /= (2*len(transformed))
-        #Exclude the first term, because that's just the mean and we expect it to be high anyway
-        if numpy.linalg.norm(transformed[1:], numpy.inf) > threshold:
-            print "Periodic/bad behavior found in trial " + d + ", file " + f
+        freq = numpy.fft.rfftfreq(len(data), d=stepsize)
+        #Skip the first element of the Fourier transform, it corresponds to the high constant portion
+        maxarg = numpy.argmax(numpy.absolute(transformed[1:])) + 1
+        if transformed[maxarg] > threshold:
+            #print "Periodic behavior found in trial " + d + ", periodic file " + f + ", period = " + str(1/freq[maxarg])
+            periods.append(1/freq[maxarg])
             bad = True
-            break
+            #break
     if not bad:
-        print "Good behavior found in trial " + d
+        print "Steady-state behavior found in trial " + d
+    else:
+        print "Periodic behavior found in trial " + d + ", periods " + str(periods)
     os.chdir("..")
